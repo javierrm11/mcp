386310b6d3f6da14c0990f4099064577edee3ee2
Javier <javierrumo2@gmail.com>
2025-06-02 21:00:34 +0200

    Ejercicio terminado

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ async def ask(query: Query):
     return {"response": response}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="127.0.0.1", port=8080)
